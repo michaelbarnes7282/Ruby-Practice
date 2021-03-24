b65d2b29f05c9fb37dfa575995895af1a4c2864f
Michael Barnes <michaelbarnes7282@gmail.com>
2021-03-24 13:35:04 -0700

    cleaned up compress_str to make it more readable

--- Algorithmic_Strategies/05_compress_str.rb
+++ Algorithmic_Strategies/05_compress_str.rb
@@ -4,23 +4,25 @@
 
 def compress_str(str)
     new = ""
-    str.split("")
+
     i = 0
     while i < str.length
-        if str[i+1] == str[i]
-            cur = str[i]
-            count = 1
-            while str[i + 1] == cur
-                count += 1
-                i += 1
-            end
-            new += count.to_s
-            next
-        else
-            new += str[i]
+        char = str[i]
+
+        count = 0
+        while str[i] == char
+            count += 1
+            i += 1
         end
-        i += 1
+
+        if count > 1
+            new += count.to_s + char
+        else
+            new += char
+        end
+        
     end
+
     new
 end
 
@@ -28,3 +30,31 @@ p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
 p compress_str("qqqqq")         # => "5q"
 p compress_str("mississippi")   # => "mi2si2si2pi"
+
+# first iteration
+
+# def compress_str(str)
+#     new = ""
+
+#     i = 0
+#     while i < str.length
+#         if str[i+1] == str[i]
+#             cur = str[i]
+#             count = 1
+
+#             while str[i + 1] == cur
+#                 count += 1
+#                 i += 1
+#             end
+
+#             new += count.to_s
+#             next
+#         else
+#             new += str[i]
+#         end
+
+#         i += 1
+#     end
+    
+#     new
+# end
