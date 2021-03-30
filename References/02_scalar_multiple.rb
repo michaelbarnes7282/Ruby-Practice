4c0f82b28e485140420992b2c9fb2681bdea9a25
Michael Barnes <michaelbarnes7282@gmail.com>
2021-03-30 10:30:37 -0700

    finished scarlar_multiple

--- References/02_scalar_multiple.rb
+++ References/02_scalar_multiple.rb
@@ -7,7 +7,11 @@
 # to be the same before and after calling your method.
 
 def scalar_multiple!(array, multiplier)
-
+    temp = array
+    (0...temp.length).each do |i|
+        temp[i] *= multiplier
+    end
+    array = temp
 end
 
 
