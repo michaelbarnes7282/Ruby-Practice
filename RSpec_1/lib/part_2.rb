cb1b965d572755b598559290a1d2384eed06a563
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-02 13:53:55 -0700

    completed caesar_cipher

--- RSpec_1/lib/part_2.rb
+++ RSpec_1/lib/part_2.rb
@@ -19,3 +19,29 @@ def vowel_counts(str)
     end
     hash
 end
+
+def caesar_cipher(str, n)
+    hash = generate_hash()
+    ans = ""
+    str.split('').each do |c|
+        if !hash.include? c
+            ans += c
+            next
+        end
+        num = hash[c]
+        num = (num + n) % 26
+        ans += hash.key(num)
+    end
+    ans
+end
+
+def generate_hash()
+    alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    hash = {}
+    count = 0
+    alpha.each do |c|
+        hash[c] = count
+        count += 1
+    end
+    hash
+end
