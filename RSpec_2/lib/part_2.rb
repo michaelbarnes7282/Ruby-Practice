077529902ce201e8371c01d9042f3bd4ecda16ce
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-07 12:08:06 -0700

    finished part_2

--- RSpec_2/lib/part_2.rb
+++ RSpec_2/lib/part_2.rb
@@ -1,10 +1,36 @@
 def palindrome?(str)
     i = 0
-    while i < str.length
-        if str[i] != str[-i]
+    k = -1
+    while i < str.length/2
+        if str[i] != str[k]
             return false
         end
         i += 1
+        k -= 1
     end
     true
+end
+
+def substrings(str)
+    ans = []
+
+    (0...str.length).each do |i|
+        (i...str.length).each do |k|
+            ans << str[i..k]
+        end
+    end
+
+    ans
+end
+
+def palindrome_substrings(str)
+    ans = []
+
+    substrings(str).each do |word|
+        if palindrome?(word) && word.length > 1
+            ans << word
+        end
+    end
+
+    ans
 end
