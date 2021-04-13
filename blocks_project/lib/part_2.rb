2820302f4ceefd0caab3faa73df48d25ac36b8aa
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-13 16:45:27 -0700

    implemented any passing students?

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -7,3 +7,7 @@ end
 def no_valid_url?(arr)
     arr.none? { |word| ['.com', '.net', '.io', '.org'].include? ("." + word.split(".")[1]) }
 end
+
+def any_passing_students?(arr)
+    arr.any? { |student| student[:grades].sum / student[:grades].length >= 75 }
+end
