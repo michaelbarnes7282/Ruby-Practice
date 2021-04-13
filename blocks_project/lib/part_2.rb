828743797a672d6cb7c79af9fd829d9e23ec5970
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-13 16:36:57 -0700

    implemented no valid url?

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -1,3 +1,9 @@
+require "byebug"
+
 def all_words_capitalized?(arr)
     arr.all? { |word| word == word.capitalize }
+end
+
+def no_valid_url?(arr)
+    arr.none? { |word| ['.com', '.net', '.io', '.org'].include? ("." + word.split(".")[1]) }
 end
