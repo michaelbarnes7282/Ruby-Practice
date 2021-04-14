59bb6d1cf80b01955386a81f0835f5dff271bc00
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-14 12:42:10 -0700

    implemented a bootcamp class with various methods

---
+++ bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,71 @@
+class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h, k| h[k] = []}
+  end
+
+  def name
+    @name
+  end
+
+  def slogan
+    @slogan
+  end
+
+  def teachers
+    @teachers
+  end
+
+  def students
+    @students
+  end
+
+  def hire(teacher)
+    @teachers.push(teacher)
+  end
+
+  def enroll(student)
+    if students.length < @student_capacity
+        students.push(student)
+        true
+    else
+        false
+    end
+  end
+
+  def enrolled?(student)
+    students.map(&:downcase).include?(student.downcase)
+  end
+
+  def student_to_teacher_ratio
+    students.length / teachers.length
+  end
+
+  def add_grade(student, grade)
+    if enrolled?(student)
+        @grades[student].push(grade)
+        true
+    else
+        false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    enrolled = enrolled?(student)
+    num = num_grades(student)
+    if !enrolled || num == 0
+        nil
+    else
+        @grades[student].sum / num
+    end
+end
+
+end
