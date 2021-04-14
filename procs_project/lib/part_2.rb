dd506d96da0167ce2669c3f0ee4b76aa179d72d5
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-14 11:08:16 -0700

    implemented greater_proc_value

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -5,3 +5,7 @@ end
 def word_changer(str, &prc)
     str.split(" ").map { |word| prc.call(word) }.join(" ")
 end
+
+def greater_proc_value(num, prc1, prc2)
+    [prc1.call(num), prc2.call(num)].max
+end
