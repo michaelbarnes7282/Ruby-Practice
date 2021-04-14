6d1e53feb37e32c862e91575c882e3289e2e36f7
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-14 12:13:00 -0700

    created a dog class with various methods

---
+++ dog_project/lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(number)
+        @age = number
+    end
+
+    def bark
+        @age > 3 ? @bark.upcase : @bark.downcase
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(food)
+        @favorite_foods.each { |fave| return true if fave.downcase == food.downcase }
+        false
+    end
+
+end
