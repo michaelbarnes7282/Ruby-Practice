d9657e73c29432bb71e0225706dae6f5d488ac8a
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-14 11:28:46 -0700

    changed i%2 == 0? to i.even? to improve readability

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -19,5 +19,5 @@ def and_selector(arr, prc1, prc2)
 end
 
 def alternating_mapper(arr, prc1, prc2)
-    arr.each_with_index.map { |ele, i| i%2 == 0 ? prc1.call(ele) : prc2.call(ele) }
+    arr.each_with_index.map { |ele, i| i.even? ? prc1.call(ele) : prc2.call(ele) }
 end
