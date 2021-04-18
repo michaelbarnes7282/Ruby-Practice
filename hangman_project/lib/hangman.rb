ff6dc437777fbce1f705e8cd7af13e97275d5ee2
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-18 13:34:43 -0700

    set up initialize method for hangman

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -1,4 +1,15 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize()
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
 
 end
