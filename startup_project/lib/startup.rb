4b577efe60c03a0d750f0cac7229a9204ac83caf
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-27 11:55:33 -0700

    implemented the startup class to pass all Part2 specs

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -2,4 +2,47 @@ require "employee"
 
 class Startup
 
+    attr_reader :name, :funding, :salaries, :employees
+
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        salaries.include? title
+    end
+
+    def >(startup)
+        @funding > startup.funding
+    end
+
+    def hire(employee_name, title)
+        if valid_title?(title)
+            @employees.push(Employee.new(employee_name, title))
+        else
+            raise "Invalid Title"
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        salary = @salaries[employee.title]
+        if @funding > salary
+            employee.pay(salary)
+            @funding -= salary
+        else
+            raise "Not enough funding"
+        end
+    end
+
+    def payday
+        employees.each { |employee| pay_employee(employee) }
+    end 
+
 end
