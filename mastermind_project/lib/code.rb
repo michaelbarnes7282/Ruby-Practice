7103cdf6e5afd4cd71e9b70aec08eb30ea4e96fb
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-28 12:26:42 -0700

    implemented the code class to pass all Part 1 specs

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,4 +6,36 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(arr)
+    arr.all? { |c| POSSIBLE_PEGS.include? c.upcase }
+  end
+
+  def initialize(pegs)
+    if Code.valid_pegs?(pegs)
+      @pegs = pegs.map(&:upcase)
+    else
+      raise "Pegs are not valid"
+    end
+  end
+
+  def self.random(length)
+    ans = []
+    length.times {ans << POSSIBLE_PEGS.keys.sample }
+    Code.new(ans)
+  end
+
+  def self.from_string(string)
+    Code.new(string.split(""))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+
 end
