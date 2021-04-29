e82cb305863e482ec686730297fed87daba05cd4
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-28 18:50:55 -0700

    implemented Mastermind class to pass all Part 3 specs

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
+require "byebug"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
 
+    def print_matches(code) 
+        p "Exact Matches: #{@secret_code.num_exact_matches(code)}"
+        p "Near Matches: #{@secret_code.num_near_matches(code)}"
+    end
+
+    def ask_user_for_guess
+        p "Enter a code"
+        input = gets.chomp
+        guess = Code.from_string(input)
+
+        print_matches(guess)
+        @secret_code == guess
+    end
 end
