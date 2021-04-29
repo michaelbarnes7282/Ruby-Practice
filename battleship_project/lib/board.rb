88dd44dd329b4ee1f63f2be330583d6ffa06ae4a
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-29 11:53:12 -0700

    implmented the Board class to pass all Part 1 specs

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,26 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n){Array.new(n, :N)}
+    @size = n * n
+  end
+
+  def [](arr)
+    @grid[arr[0]][arr[1]]
+  end
+
+  def []=(pos, value)
+    @grid[pos[0]][pos[1]] = value
+  end
+
+  def num_ships
+    count = 0
+    @grid.each do |sub|
+        sub.each do |g|
+            count += 1 if g == :S
+        end
+    end
+    count
+  end
 end
