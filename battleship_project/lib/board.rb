4ea183c4467479b3e0ad4657e71ea8a0d889a8fc
Michael Barnes <michaelbarnes7282@gmail.com>
2021-04-29 13:05:17 -0700

    implemented board class to pass part2 specs

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,10 +1,16 @@
 class Board
-  attr_reader :size
+    attr_reader :size
+    
+    def initialize(n)
+        @grid = Array.new(n){Array.new(n, :N)}
+        @size = n * n
+    end
 
-  def initialize(n)
-    @grid = Array.new(n){Array.new(n, :N)}
-    @size = n * n
-  end
+    def self.print_grid(grid)
+      grid.each do |row|
+          puts row.join(" ")
+      end
+    end
 
   def [](arr)
     @grid[arr[0]][arr[1]]
@@ -15,12 +21,51 @@ class Board
   end
 
   def num_ships
-    count = 0
-    @grid.each do |sub|
-        sub.each do |g|
-            count += 1 if g == :S
+    @grid.flatten.count { |ele| ele == :S}
+  end
+
+  def attack(pos)
+    if self[pos] == :S
+        self[pos] = :H
+        p "you sunk my battleship!"
+        return true
+    else
+        self[pos] = :X
+        return false
+    end
+  end
+
+  def place_random_ships
+    num = @size / 4
+    n = Math.sqrt(@size)
+    while num > 0
+        pos1 = rand(n)
+        pos2 = rand(n)
+        if self[[pos1, pos2]] != :S
+            self[[pos1, pos2]] = :S
+            num -= 1
         end
     end
-    count
   end
+
+  def hidden_ships_grid
+    @grid.map do |row|
+        row.map do |ele|
+            if ele == :S
+                :N
+            else
+                ele
+            end
+        end
+    end
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(hidden_ships_grid)
+  end
+
 end
